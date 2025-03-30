3fab2eae653da9b8f8957760c8694b09e73629b6
prabhat24 <prabhat24katiyar@gmail.com>
2025-03-30 11:36:57 +0530

    The type builtin: builtins

--- app/main.py
+++ app/main.py
@@ -1,18 +1,6 @@
 import sys
+from all_commands import *
 
-def do_exit(*args):
-    args = [ int(i) for i in args ]
-    exit(args[0])
-
-def do_echo(*args):
-    text = " ".join(args)
-    print(text)
-    
-
-valid_commands = {
-    "exit": do_exit,
-    "echo": do_echo
-}
 
 def main():
     # Uncomment this block to pass the first stage
@@ -28,11 +16,6 @@ def main():
             valid_commands[command](*args)
         else:
             print(f"{command}: command not found")
-
-def is_valid_command(command):
-    if command in ["exit", "echo"]:
-        return True
-
 
 if __name__ == "__main__":
     main()
