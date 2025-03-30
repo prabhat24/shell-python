c1c99a87ba50715b5c2dcda03e5a507862186849
prabhat24 <prabhat24katiyar@gmail.com>
2025-03-30 13:05:08 +0530

    codecrafters submit [skip ci]

--- app/all_commands.py
+++ app/all_commands.py
@@ -1,7 +1,8 @@
+import os
+
 def is_valid_command(command):
     if command in valid_commands.keys():
         return True
-
 
 def do_exit(*args):
     args = [ int(i) for i in args ]
@@ -15,10 +16,26 @@ def type_command(*args):
     arg_command = args[0]
     if is_valid_command(arg_command):
         print(f"{arg_command} is a shell builtin")
+        return
+    executable_path = search_as_executable(arg_command)
+    if executable_path:
+        print(f"{arg_command} is {executable_path}")
     else:
         print(f"{arg_command}: not found")
     
-
+def search_as_executable(command):
+    path_list = os.environ.get("PATH").split(":")
+    
+    for path in path_list:
+        if os.path.isdir(path):
+            path_files = os.listdir(path)
+            for file in path_files:
+                if file == command and \
+                        os.path.isfile(os.path.join(path, file)) \
+                        and os.access(os.path.join(path, file), os.X_OK):
+                    return os.path.join(path, file)
+    return ""
+                
 valid_commands = {
     "exit": do_exit,
     "echo": do_echo,
