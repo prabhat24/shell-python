0f8208fbf647f2ac0535f5c2e462227b20008700
prabhat24 <prabhat24katiyar@gmail.com>
2025-03-30 13:37:15 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,22 +3,22 @@ from app.all_commands import *
 import subprocess
 
 def main():
-    # Uncomment this block to pass the first stage
-    
-
     # Wait for user input
     while True:
         sys.stdout.write("$ ")
-        user_input = input()
-        command = user_input.strip().split()[0]
-        args = user_input.strip().split()[1:]
+        user_input = input().strip()
+        if not user_input:
+            continue
+        command = user_input.split()[0]
+        args = user_input.split()[1:]
         if is_valid_builtin_command(command):
             valid_commands[command](*args)
+            continue
         executable_path = search_as_executable(command)
         if executable_path:
             process = subprocess.Popen([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             stdout, stderr = process.communicate()
-            print(stdout)
+            sys.stdout.write(stdout)
         else:
             print(f"{command}: command not found")
 
