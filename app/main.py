04f7edbbbc517fae8996539cd25723f2751072db
prabhat24 <prabhat24katiyar@gmail.com>
2025-03-30 10:56:05 +0530

    The exit builtin

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 import sys
 
+def do_exit(*args):
+    args = [ int(i) for i in args ]
+    exit(args[0])
+
+valid_commands = {
+    "exit": do_exit
+}
 
 def main():
     # Uncomment this block to pass the first stage
@@ -8,8 +15,17 @@ def main():
     # Wait for user input
     while True:
         sys.stdout.write("$ ")
-        command = input()
+        user_input = input()
+        command = user_input.strip().split()[0]
+        args = user_input.strip().split()[1:]
+        if is_valid_command(command):
+            valid_commands[command](*args)
+
         print(f"{command}: command not found")
+
+def is_valid_command(command):
+    if command in ["exit"]:
+        return True
 
 
 if __name__ == "__main__":
